599ea46fb852fd3bcbebc0cabda8f5403feee9a7
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-10-09 00:01:37 +0300

    Playing around with internalization

--- src/main/java/com/example/webservice/controllers/UserController.java
+++ src/main/java/com/example/webservice/controllers/UserController.java
@@ -4,19 +4,25 @@ import com.example.webservice.exceptions.UserNotFoundException;
 import com.example.webservice.models.User;
 import com.example.webservice.services.UserDaoService;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserController {
 
+    private final MessageSource messageSource;
+
     private final UserDaoService userDaoService;
 
-    public UserController(UserDaoService userDaoService) {
+    public UserController(MessageSource messageSource, UserDaoService userDaoService) {
+        this.messageSource = messageSource;
         this.userDaoService = userDaoService;
     }
 
@@ -60,5 +66,13 @@ public class UserController {
                 toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping(path = "/morning")
+    public String morningInternationalized() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
