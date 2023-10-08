685be1001900f45dde664394dd416fb8d24ab0eb
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-10-08 22:00:11 +0300

    Improved the createUser method

--- src/main/java/com/example/webservice/controllers/UserController.java
+++ src/main/java/com/example/webservice/controllers/UserController.java
@@ -2,8 +2,11 @@ package com.example.webservice.controllers;
 
 import com.example.webservice.models.User;
 import com.example.webservice.services.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,22 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userDaoService.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+
+        /*
+
+        To the URI of the current request, I want to add a path /id and replace the variable with
+        the id of the created user
+
+         */
+
+        URI location = ServletUriComponentsBuilder.
+                fromCurrentRequest().
+                path("/{id}").
+                buildAndExpand(savedUser.getId()).
+                toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
