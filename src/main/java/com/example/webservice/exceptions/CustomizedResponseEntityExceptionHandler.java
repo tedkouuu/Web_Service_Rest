f05170d31f1228c68b7f76f81dba9814357f3ebd
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-10-08 23:20:20 +0300

    Configuring Auto Generation of Swagger Documentation

--- src/main/java/com/example/webservice/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/webservice/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -34,7 +34,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), request.getDescription(false));
-        
+
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 
     }
