a2473c08ffcf8f502ca711fb4d981cac0644e3b2
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2023-10-10 19:02:30 +0300

    Connecting REST API to H2 using JPA and Hibernate

--- src/main/java/com/example/webservice/models/User.java
+++ src/main/java/com/example/webservice/models/User.java
@@ -1,14 +1,19 @@
 package com.example.webservice.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @JsonProperty("user_name")
@@ -23,6 +28,10 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
